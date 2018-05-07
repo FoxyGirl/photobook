b4f0d6a68cbdb0f15257f80217bf9f8f5acfc418
Elena Pavlova <lenlho22@gmail.com>
2018-05-07 16:58:38 +0300

    Create slider by Arrows and translationX

--- js/my-slider.js
+++ js/my-slider.js
@@ -1,124 +1,43 @@
 /**
- * Created by FoxyGirl on 22.10.2017.
+ * Created by FoxyGirl on 07.05.2018.
  */
 /**** Slider ****/
 (function () {
     'use strict';
 
-    var slider = document.getElementById('profSlider');
-    console.log('slider = ' + slider);
-    var sliderContent = slider.querySelector('.slider__content').getElementsByClassName('slider__item'),
-        controlBlock = slider.querySelector('.slider__controls'),
-        sliderControls = controlBlock.getElementsByTagName('i'),
-        prevSlide = slider.querySelector('.slider__item--active'),
-        activeControl,
-        prevActiveControl,
-        zIndexSlider,
-        prevArrow = slider.querySelector('.slider__arrow--prev'),
-        nextArrow = slider.querySelector('.slider__arrow--next');
+    console.log('!!!');
+    sliderArrow('profSlider');
 
-    var computedStyle = getComputedStyle(prevSlide);
-    zIndexSlider = computedStyle.zIndex - 1;
-    console.log('prevSlide.style.zIndex ' + computedStyle.zIndex);
+    function sliderArrow(slideId) {
+        var slider = document.getElementById(slideId),
+            sliderContent = slider.querySelector('.slider__content'),
+            sliderItems = slider.querySelectorAll('.slider__item'),
+            prevArrow = slider.querySelector('.slider__arrow--prev'),
+            nextArrow = slider.querySelector('.slider__arrow--next'),
+            currentSlide = 0;
 
+        initArrows(currentSlide);
 
-    // Add controls for all slider items
-    while (sliderControls.length < sliderContent.length) {
-        var newControl = document.createElement('i');
-        controlBlock.appendChild(newControl);
-    }
+        nextArrow.addEventListener('click', function() {
+            currentSlide++;
+            initArrows(currentSlide);
+            if (currentSlide < sliderItems.length) {
+                sliderContent.style.transform = 'translateX(-' + currentSlide + '00%';
+            }
+        }, false);
 
-    for (var i = 0; i < sliderControls.length; i++) {
-        sliderControls[i].setAttribute('data-toggler', i);
-    }
+        prevArrow.addEventListener('click', function() {
+            currentSlide--;
+            initArrows(currentSlide);
+            if (currentSlide >= 0) {
+                sliderContent.style.transform = 'translateX(-' + currentSlide + '00%';
+            }
+        }, false);
 
-    activeControl = controlBlock.querySelector('.control--active').getAttribute('data-toggler');
-    activeControl = Number(activeControl);
-    prevActiveControl = setPrevActiveControl(activeControl);
-    sliderContent[prevActiveControl].classList.add('slider__item--prev-left');
-
-    controlBlock.addEventListener('click', changeSliderByControls);
-
-    nextArrow.addEventListener('click', function() {changeSliderByArrows('right')}, false);
-
-    prevArrow.addEventListener('click', function() {changeSliderByArrows('left')}, false);
-
-
-    /******************************************************/
-
-    function changeSliderByControls(e) {
-        var targetElem = e.target;
-        if (targetElem.tagName != 'I')  {
-            return;
-        } else {
-            activeControl = Number(targetElem.getAttribute('data-toggler'));
-            prevActiveControl = setPrevActiveControl(activeControl);
-            console.log('activeControl = ' + activeControl + ' / prevActiveControl= ' + prevActiveControl);
-            changeSlideControl(activeControl, prevActiveControl);
+        function initArrows(currentSlide) {
+            nextArrow.style.opacity = (currentSlide < sliderItems.length - 1) ? '' : '0';
+            prevArrow.style.opacity = (currentSlide < 1) ? '0' : '';
         }
-    }
-
-    function changeSlideControl(activeControl, prevActiveControl) {
-        controlBlock.querySelector('.control--active').classList.remove('control--active');
-        sliderControls[activeControl].classList.add('control--active');
-
-        changePrevSlide();
-        sliderContent[activeControl].classList.add('slider__item--active');
-    }
-
-    function changePrevSlide() {
-        if (prevSlide !== null) {
-            prevSlide.style.zIndex = '';
-        }
-        prevSlide = slider.querySelector('.slider__item--active');
-        prevSlide.classList.remove('slider__item--active');
-        prevSlide.style.zIndex = zIndexSlider;
-        console.log(slider.getElementsByClassName('slider__item--prev-left'));
-        slider.querySelector('.slider__item--prev-left').classList.remove('slider__item--prev-left');
-
-        prevSlide.classList.add('slider__item--prev-left');
-
-        if ( prevSlide.classList.contains('slider__item--left') ) {
-            prevSlide.classList.remove('slider__item--left');
-        }
-    }
-
-    function changeSliderByArrows(direction) {
-        switch (direction) {
-            case 'right' :
-                ++activeControl;
-                if (activeControl === sliderContent.length) {
-                    activeControl = 0;
-                }
-                prevActiveControl = setPrevActiveControl(activeControl);
-                console.log('prevActiveControl ' + prevActiveControl);
-                changeSlideControl(activeControl, prevActiveControl);
-
-                console.log('!!!');
-                break;
-
-            case 'left' :
-                --activeControl;
-                if (activeControl < 0) {
-                    activeControl = sliderContent.length - 1;
-                }
-                prevActiveControl = setPrevActiveControl(activeControl);
-
-                sliderContent[activeControl].classList.add('slider__item--left');
-                changeSlideControl(activeControl, prevActiveControl);
-                break;
-
-            default:
-                alert( 'Я таких значений не знаю' );
-        }
-    }
-
-    function setPrevActiveControl(activeControl) {
-        if (activeControl == 0) {
-            return sliderContent.length - 1;
-        }
-
-        return (activeControl - 1);
-    }
+    };
 
 })();
